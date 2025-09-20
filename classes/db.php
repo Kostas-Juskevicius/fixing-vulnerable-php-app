ac04f34bad97df5829726665f1e1910a4685fb3f
Kostas-Juskevicius <kostas.jusk@gmail.com>
2025-09-20 20:47:22 +0300

    db.php now uses environmental variables via $_ENV

--- classes/db.php
+++ classes/db.php
@@ -1,4 +1,9 @@
 <?php
-    $lnk = mysql_connect("127.0.0.1", "pentesterlab", "pentesterlab");
+    require __DIR__ '/vendor/autoload.php';
+
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+    $dotenv->load();
+
+    $lnk = mysql_connect("127.0.0.1", $_ENV['db_username'], $_ENV['db_password']);
     $db = mysql_select_db('cr', $lnk);
 ?>
