e958e11df75893793577235829f7c5d538866449
Kostas-Juskevicius <kostas.jusk@gmail.com>
2025-09-20 20:56:14 +0300

    Changed db.php not to use a deprecated connection to db func

--- classes/db.php
+++ classes/db.php
@@ -1,9 +1,7 @@
 <?php
-    require __DIR__ '/vendor/autoload.php';
-
+    require __DIR__ . '/vendor/autoload.php';
     $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
     $dotenv->load();
 
-    $lnk = mysql_connect("127.0.0.1", $_ENV['db_username'], $_ENV['db_password']);
-    $db = mysql_select_db('cr', $lnk);
+    $lnk = mysqli_connect("127.0.0.1", $_ENV['db_username'], $_ENV['db_password'], 'cr') or die("Connection to the database failed");
 ?>
